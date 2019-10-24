91e5bd024f533c5325283baed763db05af6773f0
OmnixTry <42980222+OmnixTry@users.noreply.github.com>
2019-10-24 22:41:23 +0300

    regex edited

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-/*let http = require('http');
+/* let http = require('http');
 let fs = require('fs');
- 
+
 let handleRequest = (request, response) => {
     response.writeHead(200, {
         'Content-Type': 'text/html'
@@ -15,30 +15,28 @@ let handleRequest = (request, response) => {
         response.end();
     });
 };
- 
+
 http.createServer(handleRequest).listen(8000);
 */
 
 const http = require("http");
 const fs = require("fs");
+
 const port = 3000;
-const server = http.createServer((seq, res)=>{
+const server = http.createServer((seq, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
+  res.setHeader("Content-Type", "text/html");
   const readStream = fs.createReadStream("Lab7.html").pipe(res);
 });
 
 server.listen(3000);
 
-
-
-
-/*const http = require('http') 
-const hostname = '127.0.0.1' 
-const port = 3000 
+/* const http = require('http')
+const hostname = '127.0.0.1'
+const port = 3000
 let fs = require('fs');
 
-const server = http.createServer((req, res) => { 
+const server = http.createServer((req, res) => {
   res.setHeader('Content-Type', 'text/html')
   fs.readFile('./index.html', null, function (error, data) {
     if (error) {
@@ -47,13 +45,13 @@ const server = http.createServer((req, res) => {
     } else {
         response.write(data);
     }
-    response.end();  
-    //consoleres.statusCode = 200 
-    //res.setHeader('Content-Type', 'text/html') 
+    response.end();
+    //consoleres.statusCode = 200
+    //res.setHeader('Content-Type', 'text/html')
     //res.sendFile("Lab7.html")
-    //res.end(path.resolve("Lab7.html")) 
-}) 
-server.listen(port, hostname, () => { 
-  console.log(`Server running at http://${hostname}:${port}/`) 
-}) 
+    //res.end(path.resolve("Lab7.html"))
+})
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`)
+})
 */
