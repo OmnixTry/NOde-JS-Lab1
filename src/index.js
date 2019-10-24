25de6d1e23cdc8c731060550fc7beabfcc8418dc
OmnixTry <42980222+OmnixTry@users.noreply.github.com>
2019-10-24 23:01:30 +0300

    ''

--- src/index.js
+++ src/index.js
@@ -1,35 +1,39 @@
-/* let http = require('http');
-let fs = require('fs');
-
-let handleRequest = (request, response) => {
-    response.writeHead(200, {
-        'Content-Type': 'text/html'
-    });
-    fs.readFile('./Lab7.html', null, function (error, data) {
-        if (error) {
-            response.writeHead(404);
-            respone.write('Whoops! File not found!');
-        } else {
-            response.write(data);
-        }
-        response.end();
-    });
-};
-
-http.createServer(handleRequest).listen(8000);
-*/
 
 const http = require("http");
 const fs = require("fs");
 
+const handleRequest = (request, response) => {
+  response.writeHead(200, {
+    "Content-Type": "text/html",
+  });
+  fs.readFile("./Lab7.html", null, (error, data) => {
+    if (error) {
+      response.writeHead(404);
+      respone.write("Whoops! File not found!");
+    } else {
+      response.write(data);
+    }
+    response.end();
+  });
+};
+
+http.createServer(handleRequest).listen(3000);
+
+
+/*
+
+const http = require("http");
+const fs = require("fs");
 const port = 3000;
-const server = http.createServer((seq, res) => {
+const server = http.createServer((seq, res)=>{
   res.statusCode = 200;
-  res.setHeader("Content-Type", "text/html");
+  res.setHeader('Content-Type', 'text/html');
   const readStream = fs.createReadStream("Lab7.html").pipe(res);
 });
 
 server.listen(3000);
+*/
+
 
 /* const http = require('http')
 const hostname = '127.0.0.1'
@@ -50,6 +54,23 @@ const server = http.createServer((req, res) => {
     //res.setHeader('Content-Type', 'text/html')
     //res.sendFile("Lab7.html")
     //res.end(path.resolve("Lab7.html"))
+})
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`)
+})
+*/
+
+/*
+const path = require("path")
+const http = require('http')
+const hostname = '127.0.0.1'
+const port = 3000
+const server = http.createServer((req, res) => {
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'text/html')
+    //res.sendFile("Lab7.html")
+    //res.end(path.resolve(Lab7.html))
+    //res.end('./Lab7.html')
 })
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`)
