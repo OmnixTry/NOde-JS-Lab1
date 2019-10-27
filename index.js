26f2fb925145d12d3976d18711e5c9c463b26775
OmnixTry <42980222+OmnixTry@users.noreply.github.com>
2019-10-27 11:58:08 +0200

    **/*.js changed to . again

--- index.js
+++ index.js
@@ -5,10 +5,9 @@ const fs = require("fs");
 const handleRequest = (request, response) => {
   response.writeHead(200, {
 
-        "Content-Type": "text/html",
+    "Content-Type": "text/html",
   });
   fs.readFile("./Lab7.html", null, (error, data) => {
-    
     if (error) {
       response.writeHead(404);
       response.write("Whoops! File not found!");
