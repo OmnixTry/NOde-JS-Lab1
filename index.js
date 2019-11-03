0dc8d240a9a18323cc55ff5a586b448986b02727
OmnixTry <42980222+OmnixTry@users.noreply.github.com>
2019-11-03 14:56:46 +0200

    response problem fixed

--- index.js
+++ index.js
@@ -26,9 +26,9 @@ const http = require('http')
 const fs = require("fs");
 //const hostname = '127.0.0.1'
 //const port = 3000
-const server = http.createServer((req, res) => {
- res.statusCode = 200
- res.setHeader("Content-Type", "text/html")
+const server = http.createServer((req, response) => {
+ response.statusCode = 200
+ response.setHeader("Content-Type", "text/html")
  fs.readFile("./Lab7.html", null, (error, data) => {
   if (error) {
     response.writeHead(404);
