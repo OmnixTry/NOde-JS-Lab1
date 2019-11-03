6b918720b4c613a8e041f30184937f371453c395
OmnixTry <42980222+OmnixTry@users.noreply.github.com>
2019-11-03 14:43:31 +0200

    remade index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const port = process.env.port || 3000; 
+/*
+const PORT = process.env.PORT || 3000; 
 const http = require("http");
 const fs = require("fs");
 
@@ -17,8 +18,31 @@ const handleRequest = (request, response) => {
     response.end();
   });
 };
+http.createServer(handleRequest).listen(PORT);
+*/
 
-http.createServer(handleRequest).listen(port);
+const PORT = process.env.PORT || 3000;
+const http = require('http')
+const fs = require("fs");
+//const hostname = '127.0.0.1'
+//const port = 3000
+const server = http.createServer((req, res) => {
+ res.statusCode = 200
+ res.setHeader("Content-Type", "text/html")
+ fs.readFile("./Lab7.html", null, (error, data) => {
+  if (error) {
+    response.writeHead(404);
+    response.write("Whoops! File not found!");
+  } else {
+    response.write(data);
+  }
+  response.end();
+  });
+ //res.end('Hi there!\n')
+})
+server.listen(port, hostname, () => {
+ console.log(`Server running`)
+})
 
 
 /*
