190667187c311000581d6d13956c86203d3dba24
OmnixTry <42980222+OmnixTry@users.noreply.github.com>
2019-11-03 14:21:33 +0200

    setting procfile and port for heroku

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-
+const port = process.env.port || 3000; 
 const http = require("http");
 const fs = require("fs");
 
@@ -18,7 +18,7 @@ const handleRequest = (request, response) => {
   });
 };
 
-http.createServer(handleRequest).listen(3000);
+http.createServer(handleRequest).listen(port);
 
 
 /*
